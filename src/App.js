b0e7201d8e0898cc918fe865b23f090f3e709034
liu.hanchun <liu.hanchun@foxmail.com>
2024-12-01 10:10:48 +0900

    v3

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1 style={{color: '#01579b'}}>React.js - v1</h1>
-<h1 style={{color: '#b71c1c'}}>React.js - v2</h1>
+      <h1 style={{color: '#b71c1c'}}>React.js - v2</h1>
+      <h1 style={{color: '#f44336'}}>React.js - v3</h1>
     </div>
   );
 }
